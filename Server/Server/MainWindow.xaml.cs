836dcf92bb258ba5284d746a8ac0c4a053da5157
Korneliusz Karbowski <karbowski.korneliusz@gmail.com>
2019-04-12 22:45:42 +0200

    Porządki

--- Server/Server/MainWindow.xaml.cs
+++ Server/Server/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Security;
 using System.Net.Sockets;
 using System.Net;
 using System.Security.Cryptography;
+using System.Threading;
 
 namespace Client
 {
@@ -28,65 +29,6 @@ namespace Client
         {
             InitializeComponent();
         }
-
-		private static readonly int iterations = 10;
-
-		public static byte[] CreateKey(string password, byte[] salt)
-		{
-			using (var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, iterations))
-				return rfc2898DeriveBytes.GetBytes(32);
-		}
-
-		private static byte[] GetSalt()
-		{
-			var salt = new byte[32];
-			using (var random = new RNGCryptoServiceProvider())
-			{
-				random.GetNonZeroBytes(salt);
-			}
-
-			return salt;
-		}
-
-		public static string Encrypt(string input, string password)
-		{
-			byte[] encrypted;
-			byte[] IV;
-			byte[] Salt = GetSalt();
-			byte[] Key = CreateKey(password, Salt);
-
-			using (Aes aesAlg = Aes.Create())
-			{
-				aesAlg.Key = Key;
-				aesAlg.Padding = PaddingMode.PKCS7;
-				aesAlg.Mode = CipherMode.CBC;
-
-				aesAlg.GenerateIV();
-				IV = aesAlg.IV;
-
-				var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-				using (var msEncrypt = new MemoryStream())
-				{
-					using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-					{
-						using (var swEncrypt = new StreamWriter(csEncrypt))
-						{
-							swEncrypt.Write(input);
-						}
-						encrypted = msEncrypt.ToArray();
-					}
-				}
-			}
-
-			byte[] combinedIvSaltCt = new byte[Salt.Length + IV.Length + encrypted.Length];
-			Array.Copy(Salt, 0, combinedIvSaltCt, 0, Salt.Length);
-			Array.Copy(IV, 0, combinedIvSaltCt, Salt.Length, IV.Length);
-			Array.Copy(encrypted, 0, combinedIvSaltCt, Salt.Length + IV.Length, encrypted.Length);
-
-			return Convert.ToBase64String(combinedIvSaltCt.ToArray());
-		}
-
 
 		private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -107,6 +49,10 @@ namespace Client
                 string filename = dlg.FileName;
                 selectedFileTextBox.Text = filename;
             }
+
+            //var abc = "witam serdecznie";
+            //System.Windows.MessageBox.Show(abc, "Aes");
+            //System.Windows.MessageBox.Show(Encrypt(abc, "haslo"), "Aes");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
@@ -151,7 +97,9 @@ namespace Client
 
                     SendingBuffer = new byte[CurrentPacketLength];
                     Fs.Read(SendingBuffer, 0, CurrentPacketLength);
-                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+                    byte[] genKey, genIV;
+                    byte[] encypted = Encryption(SendingBuffer, out genKey, out genIV);
+                    netstream.Write(encypted, 0, (int)encypted.Length);
 
                     if (progressBar.Value >= progressBar.Maximum)
                         progressBar.Value = progressBar.Minimum;
